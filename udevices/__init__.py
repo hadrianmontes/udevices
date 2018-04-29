4d981ebce784b5038b43648320ac781cf697fec3
Hadrián Montes <hadrianmontes@gmail.com>
2018-04-29 12:56:21 +0200

    Added a wrapper to load the screen

--- udevices/__init__.py
+++ udevices/__init__.py
@@ -1,4 +1,14 @@
 from .bme280 import BME280
 from .shift_register import ShiftRegister
 
+def OLED():
+    """
+    Wrapper to load an integrated ssd1306 screen
+    """
+    import machine
+    from udevices import ssd1306
+    i2c = machine.I2C(scl=machine.Pin(4), sda=machine.Pin(5))
+    return ssd1306.SSD1306_I2C(128, 64, i2c)
+    
+
 __all__ = ("BME280", "ShiftRegister")
