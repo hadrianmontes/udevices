7a2dec2836ab995eb05e444e1de19497fcf74bfd
Hadrián Montes <hadrianmontes@gmail.com>
2019-05-15 00:36:33 +0200

    Added server file

--- implementations/remote_weather/main.py
+++ implementations/remote_weather/main.py
@@ -3,6 +3,8 @@ import bluetooth
 import utime
 
 INTERVAL = 10
+SDA = 15
+SCL = 4
 temperature_char = bluetooth.Characteristic(uuid=1, flags=bluetooth.FLAG_READ)
 pressure_char = bluetooth.Characteristic(uuid=2, flags=bluetooth.FLAG_READ)
 blue = bluetooth.Bluetooth()
@@ -10,7 +12,7 @@ blue.active(True)
 blue.advertise(100, "uweather")
 blue.add_service(uuid=1, characteristics=[temperature_char, pressure_char])
 
-sensor = BME280(sda=15, scl=4)
+sensor = BME280(sda=SDA, scl=SCL)
 
 while True:
     sensor.update()
---
+++ implementations/remote_weather/server.py
@@ -0,0 +1,30 @@
+import time
+from bluepy.btle import Scanner, Peripheral
+
+scan = Scanner()
+
+found = False
+
+while not found:
+    devices = scan.scan()
+    for device in devices:
+        try:
+            if device.getScanData()[1][-1] == "uweather":
+                uweather= device
+                found = True
+                print("Device Found")
+                break
+        except IndexError:
+            continue
+    else:
+        print("Device not found Retrying")
+uweather = Peripheral(uweather.addr)
+uweather.connect(uweather.addr
+for service in uweather.getServices():
+    service_weather = service
+
+while True:
+    time.sleep(10)
+    characteristics = service_weather.getCharacteristics()
+    print("Temperature {}".format(characteristics[0].read()))
+
